1de4b3b7a9277c1b8aac21edbf875b0b38813f6c
Ayush pathak <ayushpathak851@yahoo.com>
2026-04-27 12:35:09 +0530

    Time: 268 ms (56.07%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,5 +1,1 @@
-SELECT 
-    (SELECT DISTINCT salary
-     FROM Employee
-     ORDER BY salary DESC
-     LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+select (Select distinct salary from employee order by salary desc limit 1 offset 1) as SecondHighestSalary
